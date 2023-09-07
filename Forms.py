5772892c597b54a45a36cc0d960547276f2bd5a4
May Than Hsu Aung <maythanhsuaung1111@gmail.com>
2023-09-07 12:35:16 +0800

    validation and iterating data for recommedation (still trying)

--- Forms.py
+++ Forms.py
@@ -1,31 +1,54 @@
-from wtforms import Form, StringField, SelectField, TextAreaField,IntegerField, validators,FileField
-from wtforms import DateField
-from wtforms import FileField
-from wtforms.validators import NumberRange
+# from wtforms import Form, StringField, SelectField, TextAreaField,IntegerField, validators,FileField
+# from wtforms import DateField
+# from wtforms import FileField
+# from wtforms.validators import NumberRange
+# from flask_wtf import FlaskForm
+# from werkzeug.utils import secure_filename
+
+from flask import Flask, request, render_template, flash, redirect, url_for
 from flask_wtf import FlaskForm
-from werkzeug.utils import secure_filename
+from wtforms import IntegerField, SelectMultipleField, SelectField
+from wtforms.validators import DataRequired, NumberRange, AnyOf
 
 
-class CreateRecipeForm(Form):
-    reward_name = StringField('Reward Name:', [validators.Length(min=1, max=20), validators.DataRequired()])
-    points_required = IntegerField('Points Required:', [validators.DataRequired(), validators.NumberRange(min=1, max=1000)])
-    points_expiry = DateField('Points expiry date:')
-    reward_type = SelectField('Reward type', [validators.DataRequired()], choices=[('', 'Select'), ('V', 'Voucher'), ('D', 'Discount')], default='')
-    description = TextAreaField('Description', [validators.Optional()])
-    reward_pic = FileField('Image', [validators.Optional()])
+# class CreateRecipeForm(Form):
+#     reward_name = StringField('Reward Name:', [validators.Length(min=1, max=20), validators.DataRequired()])
+#     points_required = IntegerField('Points Required:', [validators.DataRequired(), validators.NumberRange(min=1, max=1000)])
+#     points_expiry = DateField('Points expiry date:')
+#     reward_type = SelectField('Reward type', [validators.DataRequired()], choices=[('', 'Select'), ('V', 'Voucher'), ('D', 'Discount')], default='')
+#     description = TextAreaField('Description', [validators.Optional()])
+#     reward_pic = FileField('Image', [validators.Optional()])
+#
 
+# class preferenceform(Form):
+#     range = {"calories":[50,800], "protein":[10,100], "fat":[1,100]}  #Dictionary for their respective ranges (For Example: Minimum 50kcal and Max 800Kcal allowed)
+#     allergy = ["dairy","egg","gluten", "grain", "peanut", "seafood", "sesame", "shellfish", "soy", "sulfite", "tree nut", "wheat"]
+#     diet = ["Gluten Free","Ketogenic","Vegetarian","Lacto-vegetarian","Ovo-vegetarian","Vegan","Pescetarian","Paleo", "None"]
+#
+#     calories = IntegerField([validators.DataRequired(),validators.NumberRange(min=50, max=800)], name="maxcalories")
+#     proteins = IntegerField([validators.DataRequired(),validators.NumberRange(min=10, max=100)], name="minprotein")
+#     fats = IntegerField([validators.DataRequired(),validators.NumberRange(min=1, max=100)], name="maxfat")
+#     allergy = SelectField([validators.DataRequired()], choices = allergy, default='None',name="class")
+#     diet = SelectField([validators.DataRequired()], choices = diet, default='None',name="class")
+#
 
-class preferenceform(Form):
-    range = {"calories":[50,800], "protein":[10,100], "fat":[1,100]}  #Dictionary for their respective ranges (For Example: Minimum 50kcal and Max 800Kcal allowed)
-    allergy = ["dairy","egg","gluten", "grain", "peanut", "seafood", "sesame", "shellfish", "soy", "sulfite", "tree nut", "wheat"]
-    diet = ["Gluten Free","Ketogenic","Vegetarian","Lacto-vegetarian","Ovo-vegetarian","Vegan","Pescetarian","Paleo", "None"]
-
-    calories = IntegerField([validators.DataRequired(),validators.NumberRange(min=50, max=800)], name="maxcalories")
-    proteins = IntegerField([validators.DataRequired(),validators.NumberRange(min=10, max=100)], name="minprotein")
-    fats = IntegerField([validators.DataRequired(),validators.NumberRange(min=1, max=100)], name="maxfat")
-    allergy = SelectField([validators.DataRequired()], choices = allergy, default='None',name="class")
-    diet = SelectField([validators.DataRequired()], choices = diet, default='None',name="class")
-
+class SearchForm(FlaskForm):
+    maxcalories = IntegerField('Maximum Calories', validators=[DataRequired(), NumberRange(min=50, max=800)])
+    minprotein = IntegerField('Minimum Protein', validators=[DataRequired(), NumberRange(min=10, max=100)])
+    maxfat = IntegerField('Maximum Fat', validators=[DataRequired(), NumberRange(min=1, max=100)])
+    allergies = SelectMultipleField('Allergies', choices=[('Dairy', 'Dairy'), ('Egg', 'Egg'), ('Gluten', 'Gluten'),
+                                                          ('Grain', 'Grain'), ('Peanut', 'Peanut'), ('Seafood', 'Seafood'),
+                                                          ('Sesame', 'Sesame'), ('Shellfish', 'Shellfish'), ('Soy', 'Soy'),
+                                                          ('Sulfite', 'Sulfite'), ('Tree Nut', 'Tree Nut'), ('Wheat', 'Wheat'),
+                                                          ('None', 'None')],
+                                    validators=[AnyOf(['Dairy', 'Egg', 'Gluten', 'Grain', 'Peanut', 'Seafood',
+                                                       'Sesame', 'Shellfish', 'Soy', 'Sulfite', 'Tree Nut', 'Wheat', 'None'])])
+    preferredDiet = SelectField('Preferred Diet', choices=[('Gluten Free', 'Gluten Free'), ('Ketogenic', 'Ketogenic'),
+                                                           ('Vegetarian', 'Vegetarian'), ('Lacto-vegetarian', 'Lacto-vegetarian'),
+                                                           ('Ovo-vegetarian', 'Ovo-vegetarian'), ('Vegan', 'Vegan'),
+                                                           ('Pescetarian', 'Pescetarian'), ('Paleo', 'Paleo'), ('None', 'None')],
+                                validators=[AnyOf(['Gluten Free', 'Ketogenic', 'Vegetarian', 'Lacto-vegetarian', 'Ovo-vegetarian',
+                                                   'Vegan', 'Pescetarian', 'Paleo', 'None'])])
 
 
 
--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, flash, redirect, url_for
 import requests
+import secrets
+from Forms import SearchForm
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+
+secret_key = secrets.token_hex(32)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
 SPOONACULAR_API_KEY = '090e79deb937480d95c675344c10bf55'
+
+
 @app.route("/")
 def index():
     response = requests.get(
@@ -22,31 +33,50 @@ def index():
         # Handle API error
         return "Failed to fetch recipes from Spoonacular API."
 
-@app.route('/display_recommendations', methods =[ 'GET'])
+
+@app.route('/display_recommendations', methods=['GET', 'POST'])
 def display_recommendations():
-    # return render_template("recommendations.html")
-    response = requests.get(
-        f'https://api.spoonacular.com/recipes/complexSearch',
-        params={
-            'apiKey': SPOONACULAR_API_KEY,
-            'number': 10,  # Number of recipes to fetch
-            'sort': 'popularity',  # Sort by popularity or other criteria
-        }
-    )
-    if response.status_code == 200:
-        data = response.json()
-        values = data['results']
-        return render_template("recommendations.html", values = values)
-    else:
-        # Handle API error
-        return "Failed to fetch filtered results from Spoonacular API."
+    form = SearchForm()
+    if form.validate_on_submit():
+        # Retrieve input values
+        maxcalories = form.maxcalories.data
+        minprotein = form.minprotein.data
+        maxfat = form.maxfat.data
+        allergies = form.allergies.data
+        preferredDiet = form.preferredDiet.data
+
+        response = requests.get(
+            f'https://api.spoonacular.com/recipes/complexSearch',
+            params={
+                'apiKey': SPOONACULAR_API_KEY,
+                'diet':preferredDiet,
+                'minProtein': minprotein,
+                # 'maxFat':maxfat,
+                # 'maxCalories':maxcalories,
+                # 'excludeIngredients':allergies,
+                'number': 10,  # Number of recipes to fetch
+                'sort': 'popularity',  # Sort by popularity or other criteria
+            }
+        )
+        print(response.status_code)
+        if response.status_code == 200:
+            data = response.json()
+            print(data)
+            return render_template('recommendations.html', result=data)
+        else:
+            return 'oops no form'
+    return render_template('recommendations.html',  form = form)
+
+
 @app.route('/get_recommendations')
 def get_recommendations():
     return render_template('preferencespage/index.html')
 
+
 @app.route("/search")
 def search_page():
     return render_template('search.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
